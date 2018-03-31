d404a049a623a19734b1ad2bfe7d81da17ba780f
trevioncannon <helios.yamet@gmail.com>
2018-03-31 11:11:42 +0800

    Fine tuning

--- src/test1.ts
+++ src/test1.ts
@@ -8,8 +8,9 @@ interface IEvent {
     pageY: number;
 }
 
-const MAX_SPEED: number = 2;
-const SMOOTHING: number = 0.939;
+const MAX_SPEED: number = 3;
+const SMOOTHING: number = 0.99;
+const RELEASE: number = 0.01;
 
 export default class Test1 {
 
@@ -21,7 +22,7 @@ export default class Test1 {
         let mouseSpeed$: Rx.Observable<number> = this.createObservable(test1Content);
         let subscription: Rx.Subscription = mouseSpeed$.subscribe((state) => {
             console.log(state);
-            $("#progress-speed > div").attr("style", "width: "+state+"%");
+            $("#progress-speed > div").attr("style", "width: " + state + "%");
         });
     }
 
@@ -29,13 +30,13 @@ export default class Test1 {
      * Create an observable emitting the speed of the mouse over the provided
      * HTML element.
      */
-    private createObservable = function(this: Test1, test1Content: HTMLElement): Rx.Observable<number> {
+    private createObservable = function (this: Test1, test1Content: HTMLElement): Rx.Observable<number> {
 
         let mouseEvents$: Rx.Observable<MouseEvent> = Rx.Observable
             .fromEvent<MouseEvent>(test1Content, "mousemove");
 
         let timer$: Rx.Observable<number> = Rx.Observable
-            .interval(0)
+            .interval(5)
             .map(() => Rx.Scheduler.animationFrame.now())
             .withLatestFrom(mouseEvents$)
             .map<[number, MouseEvent], IEvent>((combo) => {
@@ -47,7 +48,11 @@ export default class Test1 {
             })
             .pairwise<IEvent>()
             .map<IEvent[], number>((pair) => this.mouseSpeed(pair[0], pair[1]))
-            .scan<number>((prev, current) => SMOOTHING * prev + (1-SMOOTHING) * current, 0)
+            .scan<number>((prev, current) => {
+                return current > 0
+                    ? SMOOTHING * prev + (1 - SMOOTHING) * current
+                    : (1 - RELEASE) * prev;
+            }, 0)
             .map<number, number>((speed) => this.normalizeSpeed(speed, MAX_SPEED));
 
         return timer$;
@@ -58,11 +63,11 @@ export default class Test1 {
      * @param previous previous mouse event
      * @param current  current mouse event
      */
-    private mouseSpeed = function(previous: IEvent, current: IEvent): number {
+    private mouseSpeed = function (previous: IEvent, current: IEvent): number {
 
         let distance: number = Math.sqrt(
-            Math.pow(previous.pageX-current.pageX, 2) +
-            Math.pow(previous.pageY-current.pageY, 2));
+            Math.pow(previous.pageX - current.pageX, 2) +
+            Math.pow(previous.pageY - current.pageY, 2));
 
         let time: number = current.timeStamp - previous.timeStamp;
         return time > 0 ? distance / time : 0;
@@ -73,7 +78,7 @@ export default class Test1 {
      * @param speed A mouse speed
      * @param maxSpeed The max speed threshold (corresponds to 100)
      */
-    private normalizeSpeed = function(speed: number, maxSpeed: number): number {
+    private normalizeSpeed = function (speed: number, maxSpeed: number): number {
         return Math.floor(Math.min(speed, maxSpeed) / maxSpeed * 100);
     };
 }

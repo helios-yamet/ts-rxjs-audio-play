f382a4ea0efd443caa8174df422e66d0b3eb8f8a
trevioncannon <helios.yamet@gmail.com>
2018-04-18 21:59:31 +0800

    Manage knob state (using behavior subject)

--- src/test4/script.ts
+++ src/test4/script.ts
@@ -12,8 +12,21 @@ export default class Test4 implements IDisposable {
 
         $("#content").html(template);
 
-        this.connectMidiController($("#connect").get(0));
-        this.dragKnob($("#knob-animation-wrapper").get(0));
+        let knob: Rx.BehaviorSubject<number> = new Rx.BehaviorSubject(0);
+
+        this.connectMidiController($("#connect").get(0), knob);
+        this.dragKnob($("#knob-animation-wrapper").get(0), knob);
+
+        knob.subscribe(
+            (state: number) => {
+                let frame: number = Math.round(state / 100 * 50);
+                $("#knob-sprites").css("transform", `translate(${-frame*200}px, 0px)`);
+                $("#knob-value h3").text(state);
+                console.log(`Subject state: ${state}`);
+            },
+            error => console.error(error),
+            () => console.log("Completed")
+        );
     }
 
     dispose(): void {
@@ -25,8 +38,9 @@ export default class Test4 implements IDisposable {
     /**
      * Connect to a MIDI input (just pick the first found)
      * @param connectBtn A button
+     * @param knobSubject A subject where new values will be emitted
      */
-    private connectMidiController = function (this: Test4, connectBtn: HTMLElement): void {
+    private connectMidiController = function (this: Test4, connectBtn: HTMLElement, knobSubject: Rx.BehaviorSubject<number>): void {
 
         this.subscription = Rx.Observable.fromEvent(connectBtn, "click")
             .take(1)
@@ -44,49 +58,44 @@ export default class Test4 implements IDisposable {
                 console.log(`Listening to input '${input.name}'...`);
                 return subject;
             })
+            .distinctUntilChanged()
             .subscribe(
-                (state: number) => {
-                    let frame: number = Math.round(state / 100 * 50);
-                    $("#knob-sprites").css("transform", `translate(${-frame*200}px, 0px)`);
-                    $("#knob-value h3").text(state);
-                    console.log(state);
-                },
+                state => knobSubject.next(state),
                 error => console.error(error)
             );
     };
 
     /**
-     * Simple drag for the button (works in absolute mode, not taking the current value into account).
+     * Simple drag for the button (new state value changes relative to current value).
      * @param connectBtn A button
+     * @param knobSubject A subject where new values will be emitted
      */
-    private dragKnob = function (this: Test4, knob: HTMLElement): void {
+    private dragKnob = function (this: Test4, htmlKnob: HTMLElement, knobSubject: Rx.BehaviorSubject<number>): void {
 
-        let mouseDown$: Rx.Observable<MouseEvent> = Rx.Observable.fromEvent(knob, "mousedown");
+        const MAX_DIST: number = 100;
+
+        let mouseDown$: Rx.Observable<MouseEvent> = Rx.Observable.fromEvent(htmlKnob, "mousedown");
         let mouseMove$: Rx.Observable<MouseEvent> = Rx.Observable.fromEvent(document, "mousemove");
         let mouseUp$: Rx.Observable<MouseEvent> = Rx.Observable.fromEvent(document, "mouseup");
-
         let mouseDrag$: Rx.Observable<number> = mouseDown$.flatMap((downEvent: MouseEvent, index: number) => {
 
             downEvent.preventDefault();
-            const MAX_DIST: number = 100;
-
-            // calculate offsets when mouse down
             let startY: number = downEvent.screenY;
+            let startValue: number = knobSubject.value;
 
             return mouseMove$.map((moveEvent) => {
 
-                let newY: number = moveEvent.screenY;
-                let dist: number = startY - newY;
-                return Math.round(Math.max(Math.min(dist, MAX_DIST) / MAX_DIST * 100, 0));
-
-            }).takeUntil(mouseUp$);
+                let dist: number = startY - moveEvent.screenY;
+                let sign: number = dist < 0 ? -1 : 1;
+                let normalized: number = Math.min(Math.abs(dist), MAX_DIST) * sign;
+                return Math.round(Math.min(MAX_DIST, Math.max(0, startValue + normalized / MAX_DIST * 100)));
+            })
+            .distinctUntilChanged()
+            .takeUntil(mouseUp$);
         });
 
-        this.subscription = mouseDrag$.subscribe((state: number) => {
-            let frame: number = Math.round(state / 100 * 50);
-            $("#knob-sprites").css("transform", `translate(${-frame*200}px, 0px)`);
-            $("#knob-value h3").text(state);
-            console.log(state);
-            });
+        mouseDrag$.subscribe(
+            state => knobSubject.next(state),
+            error => console.error(error));
     };
 }
